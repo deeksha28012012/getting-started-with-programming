c641fdbf30d87e6f19d3b5985d85e6231edf4a7a
bhavi8014 <gandhibhavi162@gmail.com>
2025-04-23 20:41:36 +0530

    programming

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello Everyone\n")
+print(3.4)
+a=5
+b="Deeksha"
+print(a)
+print(b)
+
+x= int(input("Kindly Enter A Number:"))
+y= int(input("Kindly Enter Another Number:"))
+z= x*y
+print(z)
